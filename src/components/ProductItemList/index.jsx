f13b037136bc62b2c96550aef30196d840612ca9
GabrielFunatsu <gabrielfunatsu@gmail.com>
2024-03-09 14:58:36 -0300

    adicionado a lista de produtos

---
+++ src/components/ProductItemList/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { ProductItemCard } from "../ProductItemCard";
+
+const ProductItemList = ({ products }) => {
+  const MemoizedProductItemCard = React.memo(ProductItemCard);
+  return (
+    <ul>
+      {products.map((product) => {
+        return <MemoizedProductItemCard key={product.id} product={product} />;
+      })}
+    </ul>
+  );
+};
+
+ProductItemList.propTypes = {
+  products: PropTypes.array.isRequired,
+};
+
+export { ProductItemList };
